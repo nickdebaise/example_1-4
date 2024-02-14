5db82f12f2a517ee4d0b5fd07743383dcc6fcdd1
Nick DeBaise <nickdebaise@gmail.com>
2024-02-14 21:13:43 +0000

    motor complete state machine

--- modules/wipers/wipers.cpp
+++ modules/wipers/wipers.cpp
@@ -5,10 +5,23 @@
 #include "wipers.h"
 #include "ignition.h"
 #include "display.h"
-#include "motor.h"
+#include "main.h"
 #include <string>
 
 //=====[Declaration of private defines]========================================
+
+#define PERIOD 0.02
+#define DUTY_STOP 0.075
+
+#define HI_DUMIN 0.07
+#define HI_DUMAX 0.08
+#define HD1_TIME 100
+#define HD2_TIME 100
+
+#define LO_DUMIN 0.0734
+#define LO_DUMAX 0.0767
+#define LD1_TIME 200
+#define LD2_TIME 200
 
 //=====[Declaration of private data types]=====================================
 
@@ -20,6 +33,10 @@ typedef enum {
    SHORT, MEDIUM, LONG
 } intervalSelector_t;
 
+typedef enum {
+    IDLE, HI_DIR1, HI_DIR2, LO_DIR1, LO_DIR2, INT_DIR1, INT_DIR2, INT_WAIT
+} motorState_t;
+
 //=====[Declaration and initialization of public global objects]===============
 
 // Inputs
@@ -28,6 +45,8 @@ AnalogIn wiperModeSelector(A0);
 AnalogIn intervalModeSelector(A1);
 
 // Outputs
+PwmOut wiperMotor(PF_9);
+DigitalOut blueLED(LED1);
 
 //=====[Declaration of external public global variables]=======================
 
@@ -37,6 +56,9 @@ AnalogIn intervalModeSelector(A1);
 
 wiperSelector_t selectedMode = W_OFF;
 intervalSelector_t selectedIntMode = SHORT;
+motorState_t motorState = IDLE;
+
+int accumulatedTime = 0;
 
 //=====[Declarations (prototypes) of private functions]========================
 
@@ -45,6 +67,7 @@ void readIntervalSelectorPot();
 void displaySelectedMode();
 string modeToString();
 string intervalToString();
+int mapIntervalToMs(intervalSelector_t intervalMode);
 
 //=====[Implementations of public functions]===================================
 
@@ -55,34 +78,119 @@ void checkWiperSubsystem();
 
 void wipersInit() {
     displayInit();
+    blueLED = OFF;
     displayCharPositionWrite ( 0,0 );
     displayStringWrite( "Mode:" );
     displayCharPositionWrite ( 0,1 );
     displayStringWrite( "Speed:" );
+    wiperMotor.period(PERIOD);
 }
 
 /***
     Run logic for checking wiper subsystem
 ***/
 void checkWiperSubsystem() {
+
+    readWiperSelectorPot();
+    readIntervalSelectorPot();
     if(isEngineOn()) {
-        readWiperSelectorPot();
-
-        if(selectedMode == INT) {
-            
-        } else if(selectedMode == LO) {
-            performLowSpeedCycle();
-        } else if(selectedMode == HI) {
-            performHighSpeedCycle();  
-        } else {
-            // do nothing
-        }
-
-    } else {
-        selectedMode = W_OFF;
+        displaySelectedMode();
     }
 
-    displaySelectedMode();
+    switch(motorState) {
+        case IDLE:
+            accumulatedTime = 0;
+            if(!isEngineOn() || selectedMode == OFF) {
+                motorState = IDLE;
+            } else {
+                if(selectedMode == HI) {
+                    motorState = HI_DIR1;
+                } else if(selectedMode == LO) {
+                    motorState = LO_DIR1;
+                } else if(selectedMode == INT) {
+                    motorState = INT_DIR1;
+                }
+            }
+            wiperMotor.write(DUTY_STOP);
+            blueLED = OFF;
+            break;
+        case HI_DIR1:
+            blueLED = ON;
+            if(accumulatedTime >= HD1_TIME) {
+                motorState = HI_DIR2;
+            }
+
+            accumulatedTime += SYSTEM_DELAY_TIME;
+
+            wiperMotor.write(HI_DUMIN);
+            break;
+        case HI_DIR2:
+            blueLED = OFF;
+            if(accumulatedTime >= HD2_TIME + HD1_TIME) {
+                if(selectedMode == HI) {
+                    accumulatedTime = 0;
+                    motorState = HI_DIR1;
+                } else {
+                    motorState = IDLE;
+                }
+            }
+
+            accumulatedTime += SYSTEM_DELAY_TIME;
+
+            wiperMotor.write(HI_DUMAX);
+            break;
+        case LO_DIR1:
+            if(accumulatedTime >= LD1_TIME) {
+                motorState = LO_DIR2;
+            }
+
+            accumulatedTime += SYSTEM_DELAY_TIME;
+
+            wiperMotor.write(LO_DUMIN);
+            break;
+        case LO_DIR2:
+            if(accumulatedTime >= LD2_TIME + LD1_TIME) {
+                if(selectedMode == LO) {
+                    accumulatedTime = 0;            
+                    motorState = LO_DIR1;
+                } else {
+                    motorState = IDLE;
+                }
+            }
+
+            accumulatedTime += SYSTEM_DELAY_TIME;
+
+            wiperMotor.write(LO_DUMAX);
+            break; 
+        case INT_DIR1:
+            if(accumulatedTime >= LD1_TIME) {
+                motorState = INT_DIR2;
+            }
+            accumulatedTime += SYSTEM_DELAY_TIME;
+            wiperMotor.write(LO_DUMIN);
+            break;
+        case INT_DIR2:
+            if(accumulatedTime >= LD2_TIME + LD1_TIME) {
+                motorState = INT_WAIT;
+            }
+            accumulatedTime += SYSTEM_DELAY_TIME;
+            wiperMotor.write(LO_DUMAX);
+            break;
+        case INT_WAIT:
+            if(!isEngineOn() || selectedMode != INT) {
+                motorState = IDLE;
+            }
+
+            if(accumulatedTime >= LD1_TIME + LD2_TIME + mapIntervalToMs(selectedIntMode)) {
+                accumulatedTime = 0;
+                motorState = INT_DIR1;
+            }
+
+            accumulatedTime += SYSTEM_DELAY_TIME;
+
+            wiperMotor.write(DUTY_STOP);
+    }
+
 }
 
 void displaySelectedMode() {
@@ -139,5 +247,16 @@ string intervalToString() {
             return "Medium";
         default:
             return "Long";
+    }
+}
+
+int mapIntervalToMs(intervalSelector_t intervalMode) {
+    switch(intervalMode) {
+        case SHORT:
+            return 3 * 1000;
+        case MEDIUM:
+            return 6 * 1000;
+        default: 
+            return 9 * 1000;
     }
 }
